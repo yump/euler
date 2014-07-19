8d4dcaebdda5e2887e0ef279f8f93dcbf2b1c6d4
Russell Haley <rhaley@lily>
2014-07-19 12:56:10 -0500

    Add problem 25

---
+++ p25.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Copyright (C) 2014 Russell Haley
+# 
+# This file is part of euler.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The Fibonacci sequence is defined by the recurrence relation:
+
+    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+
+Hence the first 12 terms will be:
+
+    F1 = 1
+    F2 = 1
+    F3 = 2
+    F4 = 3
+    F5 = 5
+    F6 = 8
+    F7 = 13
+    F8 = 21
+    F9 = 34
+    F10 = 55
+    F11 = 89
+    F12 = 144
+
+The 12th term, F12, is the first term to contain three digits.
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+from numerology import fibonacci
+
+def getanswer():
+    # 1000 digits in decimal representation means divisible by 10^999
+    div = 10**999
+    for termx, term in enumerate(fibonacci()):
+        if term // div > 0:
+            return termx+1 # Fibonacci indices start at 1
+
+if __name__ == "__main__":
+    print(getanswer())
